7caf55a034dc5b23b26e781245cfe3f3bd2c580f
Rahim <zowangrm@mail.uc.edu>
2026-04-12 21:44:21 -0400

    Scaffolded the project and made clear which files will be present

--- FinalProjectApp_CinemaDiary/Controllers/HomeController.cs
+++ FinalProjectApp_CinemaDiary/Controllers/HomeController.cs
@@ -18,6 +18,21 @@ namespace FinalProjectApp_CinemaDiary.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MovieThoughts()
+        {
+            return View();
+        }
+
+        public IActionResult RealFacts()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
